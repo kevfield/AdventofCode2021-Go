6a47d5058dc758c706d1d4514c2fc39495e02289
Kev Field <kevfield@gmail.com>
2021-12-01 19:28:19 +0000

    completed day1 part2

--- day01/src/main.go
+++ day01/src/main.go
@@ -19,6 +19,9 @@ func main() {
 
 	fmt.Println("p1 Answer", p1Answer)
 
+	p2Answer := sumofThree(puzzleInput)
+	fmt.Println("p2 answer", p2Answer)
+
 }
 
 // read a file from an input and return into a slice of strings
@@ -53,19 +56,49 @@ func inputFlags() (string, string) {
 	return flagfile, partid
 }
 
-func higherorLower(input []string) int {
+func higherorLower(holInput []string) int {
 
 	var higherFound int
 	var valOne int
 	var valTwo int
 
-	for i := 0; i < len(input)-1; i++ {
-		valOne, _ = strconv.Atoi(input[i+1])
-		valTwo, _ = strconv.Atoi(input[i])
+	for i := 0; i < len(holInput)-1; i++ {
+		valOne, _ = strconv.Atoi(holInput[i+1])
+		valTwo, _ = strconv.Atoi(holInput[i])
 		if valOne > valTwo {
 			higherFound++
 		}
 	}
 
 	return higherFound
+}
+
+func sumofThree(sotInput []string) int {
+
+	lengthofSlice := len(sotInput)
+	noFurther := lengthofSlice - 2
+	var totalFound int
+	var previousSum int
+	var currentSum int
+
+	for i := 0; i < len(sotInput); i++ {
+		if i < noFurther {
+			valOne, _ := strconv.Atoi(sotInput[i])
+			valTwo, _ := strconv.Atoi(sotInput[i+1])
+			valThree, _ := strconv.Atoi(sotInput[i+2])
+
+			currentSum = valOne + valTwo + valThree
+		}
+		if i != 0 {
+			if currentSum > previousSum {
+				totalFound++
+			}
+
+		} else {
+			previousSum = currentSum
+		}
+		previousSum = currentSum
+	}
+
+	return totalFound
 }
