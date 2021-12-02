c663532bc7a4c2fa6f02b998af076922468381b1
Kev Field <kevfield@gmail.com>
2021-12-02 14:01:50 +0000

    day2 part 2 done

--- day02/src/main.go
+++ day02/src/main.go
@@ -16,6 +16,8 @@ func main() {
 	//calculateDepth(puzzleInput)
 	fmt.Println("P1 Answer", calculateDepth(puzzleInput))
 
+	fmt.Println("P2 Answer", calculateAim(puzzleInput))
+
 }
 
 func calculateDepth(cdInput []string) int {
@@ -34,6 +36,33 @@ func calculateDepth(cdInput []string) int {
 		} else if moveCommand[0] == "up" {
 			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
 			depthPos = depthPos - currdirectionamountInt
+		}
+	}
+	finalPosition = depthPos * horizontalPos
+
+	return finalPosition
+}
+
+func calculateAim(cdInput []string) int {
+	var finalPosition, horizontalPos, depthPos, aim, currdirectionamountInt int
+
+	for i := 0; i < len(cdInput); i++ {
+
+		moveCommand := strings.Split(cdInput[i], " ")
+
+		if moveCommand[0] == "forward" {
+			// forward X does two things:
+			// It increases your horizontal position by X units.
+			// It increases your depth by your aim multiplied by X.
+			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
+			horizontalPos = horizontalPos + currdirectionamountInt
+			depthPos = depthPos + aim*currdirectionamountInt
+		} else if moveCommand[0] == "down" {
+			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
+			aim = aim + currdirectionamountInt
+		} else if moveCommand[0] == "up" {
+			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
+			aim = aim - currdirectionamountInt
 		}
 	}
 	finalPosition = depthPos * horizontalPos
