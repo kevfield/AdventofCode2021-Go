359b06a3d575d6168f02899be8183f841174ced7
Kev Field <kevfield@gmail.com>
2021-12-02 15:16:41 +0000

    tidied up code and removed comments

--- day02/src/main.go
+++ day02/src/main.go
@@ -8,20 +8,16 @@ import (
 
 func main() {
 
-	// get file location from user
+	// get file location
 	fileLoc, _ := inputFlags()
-
+	// get puzzle input, add to slice of strings
 	puzzleInput, _ := readFile(fileLoc)
 
-	//calculateDepth(puzzleInput)
-	fmt.Println("P1 Answer", calculateDepth(puzzleInput))
-
-	fmt.Println("P2 Answer", calculateAim(puzzleInput))
-
+	fmt.Println("P1 Answer =", calculateDepth(puzzleInput), "\nP2 Answer =", calculateAim(puzzleInput))
 }
 
 func calculateDepth(cdInput []string) int {
-	var finalPosition, horizontalPos, depthPos, currdirectionamountInt int
+	var horizontalPos, depthPos, currdirectionamountInt int
 
 	for i := 0; i < len(cdInput); i++ {
 
@@ -29,43 +25,36 @@ func calculateDepth(cdInput []string) int {
 
 		if moveCommand[0] == "forward" {
 			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
-			horizontalPos = horizontalPos + currdirectionamountInt
+			horizontalPos += currdirectionamountInt
 		} else if moveCommand[0] == "down" {
 			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
-			depthPos = depthPos + currdirectionamountInt
+			depthPos += currdirectionamountInt
 		} else if moveCommand[0] == "up" {
 			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
-			depthPos = depthPos - currdirectionamountInt
+			depthPos -= currdirectionamountInt
 		}
 	}
-	finalPosition = depthPos * horizontalPos
-
-	return finalPosition
+	return depthPos * horizontalPos
 }
 
 func calculateAim(cdInput []string) int {
-	var finalPosition, horizontalPos, depthPos, aim, currdirectionamountInt int
+	var horizontalPos, depthPos, aim, currdirectionamountInt int
 
 	for i := 0; i < len(cdInput); i++ {
 
 		moveCommand := strings.Split(cdInput[i], " ")
 
 		if moveCommand[0] == "forward" {
-			// forward X does two things:
-			// It increases your horizontal position by X units.
-			// It increases your depth by your aim multiplied by X.
 			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
-			horizontalPos = horizontalPos + currdirectionamountInt
-			depthPos = depthPos + aim*currdirectionamountInt
+			horizontalPos += currdirectionamountInt
+			depthPos += aim * currdirectionamountInt
 		} else if moveCommand[0] == "down" {
 			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
 			aim = aim + currdirectionamountInt
 		} else if moveCommand[0] == "up" {
 			currdirectionamountInt, _ = strconv.Atoi(moveCommand[1])
-			aim = aim - currdirectionamountInt
+			aim -= currdirectionamountInt
 		}
 	}
-	finalPosition = depthPos * horizontalPos
-
-	return finalPosition
+	return depthPos * horizontalPos
 }
