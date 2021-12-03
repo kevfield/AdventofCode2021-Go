b4e35411a545d4adb25c248ffa9f8e9895e16f96
Kev Field <kevfield@gmail.com>
2021-12-03 15:40:35 +0000

    broken day3 part2

--- day03/src/day03.go
+++ day03/src/day03.go
@@ -12,11 +12,14 @@ func main() {
 	// get puzzle input, add to slice of strings
 	puzzleInput, _ := readFile(fileLoc)
 
-	fmt.Println("P1 Answer =", commonBits(puzzleInput))
+	p1Answer, oneMap, zeroMap := commonBits(puzzleInput)
+	lifeSupport(puzzleInput, oneMap, zeroMap)
+
+	fmt.Println("P1 Answer =", p1Answer) //, "\nP2 Answer  =", p2Answer)
 
 }
 
-func commonBits(cbInput []string) int {
+func commonBits(cbInput []string) (int, map[int]int, map[int]int) {
 	// make a map to count the bits
 	onebitsMap := make(map[int]int)
 	zerobitsMap := make(map[int]int)
@@ -48,5 +51,66 @@ func commonBits(cbInput []string) int {
 	gammaCalc, _ := strconv.ParseInt(string(gammaSlice), 2, 64)
 	epsilonCalc, _ := strconv.ParseInt(string(epsilonSlice), 2, 64)
 
-	return int(gammaCalc) * int(epsilonCalc)
+	return int(gammaCalc) * int(epsilonCalc), onebitsMap, zerobitsMap
+}
+
+func lifeSupport(lsInput []string, lsoneMap map[int]int, lszeroMap map[int]int) {
+	oxygenSlice := make([]string, 0)
+	oxygentempSlice := make([]string, 0)
+	co2Slice := make([]string, 0)
+	co2tempSlice := make([]string, 0)
+	//keepLooping := true
+
+	// loop through runes of string
+	for i := 0; i < len(lsInput[0]); i++ {
+		// check if our slice has one entry
+		if len(oxygenSlice) != 1 {
+			// loop through same rune on each line of input
+			for j := 0; j < len(lsInput); j++ {
+				// check map for most common
+				if lsoneMap[j] >= lszeroMap[j] {
+					if lsInput[j][0] == '1' {
+						oxygentempSlice = append(oxygentempSlice, lsInput[j])
+					}
+				} else {
+					if lsInput[j][0] == '0' {
+						oxygentempSlice = append(oxygentempSlice, lsInput[j])
+					}
+				}
+			}
+
+		}
+		fmt.Println("oxslice", oxygenSlice)
+		oxygenSlice = append(oxygenSlice, oxygentempSlice[0])
+		oxygentempSlice = nil
+	}
+
+	// loop through runes of string
+	for i := 0; i < len(lsInput[0]); i++ {
+		// check if our slice has one entry
+		if len(co2Slice) != 1 {
+			// loop through same rune on each line of input
+			for j := 0; j < len(lsInput); j++ {
+				// check map for most common
+				if lszeroMap[j] >= lsoneMap[j] {
+					if lsInput[j][0] == '0' {
+						co2tempSlice = append(co2tempSlice, lsInput[j])
+					}
+				} else {
+					if lsInput[j][0] == '1' {
+						co2tempSlice = append(co2tempSlice, lsInput[j])
+					}
+				}
+			}
+
+		}
+		fmt.Println("co2slice", co2Slice)
+		co2Slice = append(co2Slice, co2tempSlice[0])
+		co2tempSlice = nil
+	}
+
+	//oxygenCalc, _ := strconv.Atoi(oxygenSlice[0])
+	//co2Calc, _ := strconv.Atoi(co2Slice[0])
+
+	//return oxygenCalc * co2Calc
 }
